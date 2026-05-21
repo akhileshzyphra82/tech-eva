728882678be58fc2987c89029167f496c5e19981
Akhilesh Kannaujiya <akhileshkannaujiya@Akhileshs-MacBook-Air.local>
2026-05-21 21:37:46 +0530

    t

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ $basePath = $basePath ?? '';
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.44.0/tabler-icons.min.css">
-<link rel="stylesheet" href="<?= $basePath ?>css/style.css">
+<link rel="stylesheet" href="<?= $basePath ?>css/style.css?v=<?= time() ?>">
 </head>
 <body>
 
